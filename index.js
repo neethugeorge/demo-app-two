3649f4a49798f087b011fab7d9b135d6fd4fbd92
amal <amalv@qburst.com>
2017-06-12 12:46:13 +0530

    changed path from / to /serviceB

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ server.get('/health', function (req, res, next) {
   return next();
 });
 
-server.get('/', function (req, res, next) {
+server.get('/serviceB', function (req, res, next) {
   res.send(response2);
   return next();
 });
